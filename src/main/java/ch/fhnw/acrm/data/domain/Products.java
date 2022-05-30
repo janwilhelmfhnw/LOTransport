57061f45354ec8ea3b46ed1949e84485b0ebf04f
phgag <ph.gagneux@gmail.com>
2022-05-30 13:28:03 +0200

    added product classes

---
+++ src/main/java/ch/fhnw/acrm/data/domain/Products.java
@@ -0,0 +1,68 @@
+package ch.fhnw.acrm.data.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Products {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private double price;
+    private double palletSize;
+
+    public Products(Long id, String name, double price, double palletSize) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.palletSize = palletSize;
+    }
+
+    public Products() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPalletSize() {
+        return palletSize;
+    }
+
+    public void setPalletSize(double palletSize) {
+        this.palletSize = palletSize;
+    }
+
+    @Override
+    public String toString() {
+        return "Products{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", palletSize=" + palletSize +
+                '}';
+    }
+}
