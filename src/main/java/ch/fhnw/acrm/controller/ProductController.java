6c6cfd7e7e6a9e2da0b7dcbed0439b12e698855e
phgag <ph.gagneux@gmail.com>
2022-05-31 15:53:25 +0200

    created parameter

--- src/main/java/ch/fhnw/acrm/controller/ProductController.java
+++ src/main/java/ch/fhnw/acrm/controller/ProductController.java
@@ -1,10 +1,27 @@
 package ch.fhnw.acrm.controller;
 
 
+import ch.fhnw.acrm.data.repository.ProductRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity getAllProducts() {
+        return ResponseEntity.ok(this.productRepository.findAll());
+
+    }
+
+
 }
