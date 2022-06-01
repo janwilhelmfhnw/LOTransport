724dab8425dbf5a95914ced464c2fa3553c5deff
kenanahmetasevic <kenan.ahmetasevic@students.fhnw.ch>
2022-06-01 19:24:40 +0200

    Database connection

---
+++ src/main/java/ch/fhnw/acrm/data/domain/ProductData.java
@@ -0,0 +1,50 @@
+package ch.fhnw.acrm.data.domain;
+
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.sql.*;
+
+
+public class ProductData {
+
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/db9gl717l6jetr";
+        String uname = "trjjnfbrmizoql";
+        String password = "909d7787c99319cdf1f1e15ed5329a2bcfed409371985741e3aa20d43534b7f0";
+        String query = "select * from product";
+
+
+        try {
+            Connection con = DriverManager.getConnection(url, uname, password);
+            System.out.println("Connected");
+            Statement statement = con.createStatement();
+            ResultSet result = statement.executeQuery(query);
+
+            while (result.next()) {
+                String palletSize = "";
+                String price = "";
+
+                for (int i = 1; i < 5; i++){
+                    palletSize = result.getString(3);
+                    price = result.getString(4);
+                }
+                System.out.println("Pallet Size: " + palletSize + "\n" + "Price: " + price);
+            }
+
+        }
+        catch (SQLException e) {
+            System.out.println("not connected");
+        }
+
+
+    }
+
+
+
+}
