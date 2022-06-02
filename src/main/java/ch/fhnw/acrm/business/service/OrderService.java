508d13d46adddab9403efcb54631ffbf4c98d152
phgag <ph.gagneux@gmail.com>
2022-06-02 13:48:41 +0200

    form db test

--- src/main/java/ch/fhnw/acrm/business/service/OrderService.java
+++ src/main/java/ch/fhnw/acrm/business/service/OrderService.java
@@ -1,11 +1,26 @@
 package ch.fhnw.acrm.business.service;
 
+import ch.fhnw.acrm.data.domain.Agent;
+import ch.fhnw.acrm.data.domain.Orders;
 import ch.fhnw.acrm.data.repository.OrdersRepository;
 import ch.fhnw.acrm.data.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.validation.Valid;
+@Service
 public class OrderService {
 
     @Autowired
     private OrdersRepository orderRepository;
+
+
+    public void saveOrders(Orders orders) throws Exception {
+        if (orders.getId() == null)
+
+        //orders.setPassword(passwordEncoder.encode(agent.getPassword()));
+        orderRepository.save(orders);
+    }
+
+
 }
--- src/main/java/ch/fhnw/acrm/controller/ProductController.java
+++ src/main/java/ch/fhnw/acrm/controller/ProductController.java
@@ -1,26 +1,40 @@
 package ch.fhnw.acrm.controller;
 
 
+import ch.fhnw.acrm.business.service.OrderService;
 import ch.fhnw.acrm.business.service.ProductService;
 import ch.fhnw.acrm.data.domain.Agent;
+import ch.fhnw.acrm.data.domain.Orders;
 import ch.fhnw.acrm.data.domain.Product;
 import ch.fhnw.acrm.data.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 //@RequestMapping("/product")
 public class ProductController {
 
     @Autowired
-    private ProductService productService;
+    private OrderService orderService;
 
     @GetMapping("/productOrder")
     public String getProductView(){return "user/productOrder.html";}
+
+
+    @PostMapping("/productOrder")
+    public ResponseEntity<Void> postOrders(@RequestBody Orders orders) {
+        try {
+            orderService.saveOrders(orders);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, e.getMessage());
+        }
+        return ResponseEntity.ok().build();
+    }
+
 
     private final ProductRepository productRepository;
 
