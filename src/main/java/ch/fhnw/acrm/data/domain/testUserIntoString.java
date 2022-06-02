38c1b69899637db42a1febf778feb7b44d7931f5
Jan Wilhelm <jan.wilhelm@students.fhnw.ch>
2022-06-02 10:41:06 +0200

    added address pull for API

---
+++ src/main/java/ch/fhnw/acrm/data/domain/testUserIntoString.java
@@ -0,0 +1,65 @@
+package ch.fhnw.acrm.data.domain;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class testUserIntoString {
+    public static void main(String[] args) {
+    String url = "jdbc:postgresql://ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/db9gl717l6jetr";
+    String uname = "trjjnfbrmizoql";
+    String password = "909d7787c99319cdf1f1e15ed5329a2bcfed409371985741e3aa20d43534b7f0";
+    String query = "select * from agent";
+
+
+    try {
+        Connection con = DriverManager.getConnection(url, uname, password);
+        System.out.println("Connected");
+        Statement statement = con.createStatement();
+        ResultSet result = statement.executeQuery(query);
+
+        while (result.next()) {
+            String streetname = "";
+            String  streetnumber= "";
+            String  postalcode= "";
+            String  city= "";
+
+            for (int i = 1; i < 10; i++){
+                streetname = result.getString(9);
+                streetnumber = result.getString(8);
+                postalcode = result.getString(7);
+                 city = result.getString(6);
+            }
+            System.out.println("Location: " +streetname + streetnumber + ", " +postalcode +city);
+
+            String address = "";
+
+            ArrayList<String> sList = new ArrayList<String>();
+            sList.add(streetname);
+            sList.add(streetnumber+", ");
+            sList.add(postalcode);
+            sList.add(city);
+
+
+            for (int x = 0; x < sList.size(); x++) {
+                address += sList.get(x);
+
+            }
+            System.out.println(address);
+
+        }
+
+    }
+    catch (SQLException e) {
+        System.out.println("not connected");
+    }
+
+
+}
+}
