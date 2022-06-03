12146cde3b9c0c24e5d75a965763da51599f403e
phgag <ph.gagneux@gmail.com>
2022-06-03 04:36:49 +0200

    10

--- src/main/java/ch/fhnw/acrm/controller/ProductController.java
+++ src/main/java/ch/fhnw/acrm/controller/ProductController.java
@@ -10,21 +10,43 @@ import ch.fhnw.acrm.data.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/product")
+
 public class ProductController {
 
     private final ProductService productService;
+//    @Autowired
+//    private final ProductRepository productRepository;
+    @Autowired
+    private AgentRepository agentRepository;
+    @Autowired
+    private AgentService agentService;
 
-//    @Autowired
-//    private AgentRepository agentRepository;
+
+    public ProductController(ProductService productService, ProductRepository productRepository, AgentRepository agentRepository, AgentService agentService) {
+        this.productService = productService;
+        this.agentRepository = agentRepository;
+        this.agentService = agentService;
+    }
+
+    @RequestMapping("/product")
+    public String getName(Model model){
+       Agent a = agentService.getCurrentAgent();
+        model.addAttribute("name", agentRepository.findByName(String.valueOf(a)));
+
+        return "name";
+    }
 //
-//    @Autowired
-//    private AgentService agentService;
+//    @RequestMapping("/product2"){
+//        return String
+//    }
+
+
 
 @Autowired
     public ProductController(ProductService productService) {
