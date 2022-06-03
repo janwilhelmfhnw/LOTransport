c87c9245fd35bc5f8fe78b0a37a250ceba4af8dc
phgag <ph.gagneux@gmail.com>
2022-06-03 03:13:06 +0200

    9

--- src/main/java/ch/fhnw/acrm/controller/OrderConfig.java
+++ src/main/java/ch/fhnw/acrm/controller/OrderConfig.java
@@ -1,21 +1,47 @@
 package ch.fhnw.acrm.controller;
 
 
+import ch.fhnw.acrm.data.repository.OrdersRepository;
 import ch.fhnw.acrm.data.repository.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
-public class OrderConfig {
+@Component
+public class OrderConfig implements CommandLineRunner{
 
-    @Bean
-    CommandLineRunner commandLineRunner(
-            ProductRepository repository) {
-        return  args -> {
+    private final ProductRepository productRepository;
+    private final OrdersRepository ordersRepository;
+
+    public OrderConfig(ProductRepository productRepository, OrdersRepository ordersRepository) {
+        this.productRepository = productRepository;
+        this.ordersRepository = ordersRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
 
 
-        };
+
 
     }
+
+
+
+
+
+
+
+
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            ProductRepository repository) {
+//        return  args -> {
+//
+//
+//        };
+//
+//    }
 }
--- src/main/java/ch/fhnw/acrm/data/repository/ProductRepository.java
+++ src/main/java/ch/fhnw/acrm/data/repository/ProductRepository.java
@@ -3,13 +3,14 @@ package ch.fhnw.acrm.data.repository;
 import ch.fhnw.acrm.data.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends CrudRepository<Product, Long> {
 
 //    @Query("SELECT s FROM Product s WHERE s.")
     Optional<Product> findProductById(long id);
