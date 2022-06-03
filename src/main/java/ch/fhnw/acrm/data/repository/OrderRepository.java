0464da7158b74c6698f753fde05413e41c0b6a31
Jan Wilhelm <jan.wilhelm@students.fhnw.ch>
2022-06-03 21:03:05 +0200

    bugfix

--- src/main/java/ch/fhnw/acrm/data/repository/OrderRepository.java
+++ src/main/java/ch/fhnw/acrm/data/repository/OrderRepository.java
@@ -1,12 +1,10 @@
 package ch.fhnw.acrm.data.repository;
 
-import org.springframework.core.annotation.Order;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
+/*
 
 @Repository
 public interface OrderRepository extends CrudRepository <Order, Long> {
 
 
 }
+*/
